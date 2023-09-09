a37dbef2daaad134d94e3c3a16911529c2243efe
David Mendoza Erazo <davidleomendoza300@gmail.com>
2023-09-09 17:38:51 -0500

    Data Update

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const sequelize = require('./database.js');
+const path = require('path');
 const app = express();
 const port = 3001;
 app.use(bodyParser.json());
