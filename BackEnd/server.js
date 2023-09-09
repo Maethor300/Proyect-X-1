745ac9a7af88b9018c0cd8bbeca40f701e1c3772
David Mendoza Erazo <davidleomendoza300@gmail.com>
2023-09-09 18:35:21 -0500

    Prueba

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3001;
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '..', 'build')));
+ 
 app.post('/agregarDatos', (req, res) => {
     const { id, nombre, email, telefono, opcion } = req.body;
     
@@ -35,9 +35,7 @@ app.post('/agregarDatos', (req, res) => {
       res.status(500).json({ error: 'Error al realizar la consulta' });
     }
   });
-  app.get('/miPagina', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-});
+  
   sequelize.sync()
   .then(() => {
     console.log('Base de datos sincronizada');
